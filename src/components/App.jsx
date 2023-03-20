6635a085dd8cba438d9be9ead886f998da591d8c
SergSmilya <sergsmilichenko@gmail.com>
2023-03-20 14:06:38 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,43 +1,36 @@
-import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react';
 import Contacts from './Contacts/Contacts';
 import ContactForm from './ContactForm/ContactForm';
 import { nanoid } from 'nanoid';
 import Filter from './Filter/Filter';
 
 const STORAGE_KEY = 'Contacts';
-export default class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
 
-  componentDidMount() {
-    const parseContacts = JSON.parse(localStorage.getItem(STORAGE_KEY));
+export default function App() {
+  const [filter, setFilter] = useState('');
 
-    if (localStorage.getItem(STORAGE_KEY))
-      this.setState({ contacts: parseContacts });
-  }
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
 
-  componentDidUpdate(_, prevState) {
-    const { contacts } = this.state;
-    if (contacts !== prevState.contacts) {
-      localStorage.setItem(STORAGE_KEY, JSON.stringify(contacts));
-    }
-  }
+  // useEffect(() => {
+  //   const parseContacts = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
-  onFormData = data => {
+  //   if (localStorage.getItem(STORAGE_KEY)) setContacts(parseContacts);
+  // }, []);
+
+  useEffect(() => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
+  const onFormData = data => {
     const dataNameLowerCase = data.name.toLowerCase().trim();
 
     if (
-      this.state.contacts.find(
-        el => dataNameLowerCase === el.name.toLowerCase().trim()
-      )
+      contacts.find(el => dataNameLowerCase === el.name.toLowerCase().trim())
     ) {
       alert(`Contact was added`);
       return;
@@ -46,55 +39,38 @@ export default class App extends Component {
     const id = nanoid(3);
     data = { ...data, id };
 
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, data],
-    }));
+    setContacts(s => [...s, data]);
   };
 
-  onFilterControl = value => {
-    this.setState({ filter: value });
+  const onFilterControl = value => {
+    setFilter(value);
   };
 
-  onFilterSearch = () => {
-    const { contacts, filter } = this.state;
-
+  const onFilterSearch = () => {
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter)
     );
-
-    // return contacts
-    //   .map(contact => contact.name.toLowerCase().includes(filter) && contact)
-    //   .filter(contact => contact !== false);
   };
 
-  onDeleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(({ id }) => id !== contactId),
-    }));
+  const onDeleteContact = contactId => {
+    setContacts(s => contacts.filter(({ id }) => id !== contactId));
   };
 
-  render() {
-    return (
-      <div
-        style={{
-          height: '100%',
-          display: 'block',
-          fontSize: 20,
-          color: '#010101',
-        }}
-      >
-        <h1>Phonebook</h1>
-        <ContactForm onFormData={this.onFormData} />
+  return (
+    <div
+      style={{
+        height: '100%',
+        display: 'block',
+        fontSize: 20,
+        color: '#010101',
+      }}
+    >
+      <h1>Phonebook</h1>
+      <ContactForm onFormData={onFormData} />
 
-        <h2>Contacts</h2>
-        <Filter onFilterControl={this.onFilterControl} />
-        <Contacts
-          contacts={this.onFilterSearch()}
-          onDeleteContact={this.onDeleteContact}
-        />
-      </div>
-    );
-  }
+      <h2>Contacts</h2>
+      <Filter onFilterControl={onFilterControl} />
+      <Contacts contacts={onFilterSearch()} onDeleteContact={onDeleteContact} />
+    </div>
+  );
 }
-
-// App.propTypes = {};
