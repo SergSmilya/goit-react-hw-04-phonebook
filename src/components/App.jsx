bdd75708786f1d4202b09cb1a3171a235c924750
SergSmilya <sergsmilichenko@gmail.com>
2023-03-21 16:11:09 +0200

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,32 +12,32 @@ const defaultArrayContacts = [
   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
 ];
 
+// Custom hook
+// function useLocalStorageContacts(key, defaultValue) {
+//   const [state, setState] = useState(() => {
+//     return JSON.parse(localStorage.getItem(key)) ?? defaultValue;
+//   });
+
+//   useEffect(() => {
+//     localStorage.setItem(key, JSON.stringify(state));
+//   }, [key, state]);
+
+//   return [state, setState];
+// }
+
 export default function App() {
   const [filter, setFilter] = useState('');
 
-  // Переписать useState contacts на кастомн. хук
+  // const [contacts, setContacts] = useLocalStorageContacts(
+  //   STORAGE_KEY,
+  //   defaultArrayContacts
+  // );
+
   const [contacts, setContacts] = useState(() => {
-    const parseContacts = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
-    if (localStorage.getItem(STORAGE_KEY) && parseContacts.length > 0) {
-      return parseContacts;
-    } else {
-      return defaultArrayContacts;
-    }
+    return (
+      JSON.parse(localStorage.getItem(STORAGE_KEY)) ?? defaultArrayContacts
+    );
   });
-
-  // const [contacts, setContacts] = useState([
-  //   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  //   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  //   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  //   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  // ]);
-
-  // useEffect(() => {
-  //   const parseContacts = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
-  //   if (localStorage.getItem(STORAGE_KEY)) setContacts(parseContacts);
-  // }, []);
 
   useEffect(() => {
     localStorage.setItem(STORAGE_KEY, JSON.stringify(contacts));
